829cfb4f14021f4d8c5a4faa45ca091e90b6e4b5
AntoninP21 <you@example.com>
2024-01-30 14:50:42 +0100

    ajout de l'erreur dans app.js

--- app.js
+++ app.js
@@ -19,4 +19,9 @@ require('./src/routes/createPlanet')(app);
 require('./src/routes/updatePlanet')(app);
 require('./src/routes/deletePlanet')(app);
 
+app.use(({res}) => {
+   const message = `Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.`;
+   res.status(404).json({message: message});
+});
+
 app.listen( port, () => console.log(`Notre application Node est démarrée sur http://localhost:${port}`));
